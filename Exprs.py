11978c46d6c8e9168c3661db7b2e8813a0efd53b
Taehun Kim <taehoon1018@postech.ac.kr>
2021-09-15 21:28:10 +0900

    server expr

---
+++ Exprs.py
@@ -0,0 +1,41 @@
+import os
+import argparse
+import yaml
+import copy
+
+def _args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='configs/InSPyReNet_SwinB.yaml')
+    parser.add_argument('--devices', type=str, default='0')
+    parser.add_argument('--exprs', type=int, default=4)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = _args()
+    exp_name = os.path.splitext(os.path.split(args.config)[-1])[0]
+    devices = args.devices.split(',')
+    opt = yaml.load(open(args.config), yaml.FullLoader)
+
+    os.makedirs('temp', exist_ok=True)
+    exp_tab = []
+
+    for i in range(args.exprs):
+        opt_c = copy.deepcopy(opt)
+        cfg_name = exp_name + '_expr' + str(i + 1)
+
+        ckpt_dir = opt_c['Train']['Checkpoint']['checkpoint_dir']
+        opt_c['Train']['Checkpoint']['checkpoint_dir'] = os.path.join(*os.path.split(ckpt_dir)[:-1], cfg_name)
+        opt_c['Test']['Checkpoint']['checkpoint_dir'] = os.path.join(*os.path.split(ckpt_dir)[:-1], cfg_name)
+        opt_c['Eval']['pred_root'] = os.path.join(*os.path.split(ckpt_dir)[:-1], cfg_name)
+
+        yaml.dump(opt_c, open(os.path.join('temp', cfg_name + '.yaml'), 'w'), sort_keys=False)
+        exp_tab.append((cfg_name, devices[i % len(devices)]))
+
+    for device in devices:
+        command = 'tmux new-window \"source ~/.zshrc && conda activate inspyrenet ' 
+        for exp in exp_tab:
+            if exp[1] == device:
+                command += '&& CUDA_VISIBLE_DEVICES={} python Expr.py --config {} '.format(device, os.path.join('temp', exp[0] + '.yaml'))
+        command += '\"'
+        os.system(command)
