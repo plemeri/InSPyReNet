5197f027dc538ab4b0681d2323ca752a96ca8be5
Taehun Kim <taehoon1018@postech.ac.kr>
2021-09-15 21:34:03 +0900

    server expr

--- Exprs.py
+++ Exprs.py
@@ -2,6 +2,8 @@ import os
 import argparse
 import yaml
 import copy
+import random
+import string
 
 def _args():
     parser = argparse.ArgumentParser()
@@ -13,6 +15,7 @@ def _args():
 
 if __name__ == "__main__":
     args = _args()
+    exp_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))    
     exp_name = os.path.splitext(os.path.split(args.config)[-1])[0]
     devices = args.devices.split(',')
     opt = yaml.load(open(args.config), yaml.FullLoader)
@@ -22,7 +25,7 @@ if __name__ == "__main__":
 
     for i in range(args.exprs):
         opt_c = copy.deepcopy(opt)
-        cfg_name = exp_name + '_expr' + str(i + 1)
+        cfg_name = exp_name + '_expr_' + exp_key + '_' + str(i + 1)
 
         ckpt_dir = opt_c['Train']['Checkpoint']['checkpoint_dir']
         opt_c['Train']['Checkpoint']['checkpoint_dir'] = os.path.join(*os.path.split(ckpt_dir)[:-1], cfg_name)
