31621135570b7e3b8985b831e570e8fc4c438095
Taehun Kim <taehoon1018@postech.ac.kr>
2021-10-29 20:50:52 +0900

    code update

--- install.sh
+++ install.sh
@@ -11,15 +11,31 @@ if ! [ -x "$(command -v curl)" ]; then
 fi
 
 while true; do
-    read -p "Do you wish to download datasets and backbone checkpoints? [y/n]: " yn
+    read -p "Do you wish to download datasets? [y/n]: " yn
     case $yn in
       [Yy]* )
-          curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1KkXffb1DEu1be7NO-RPUy1r2bZqJRuYl" > /dev/null
-          curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1KkXffb1DEu1be7NO-RPUy1r2bZqJRuYl" -o data.zip
+          curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1QKYG58WLDdq4ar690H5qCNiSgI1twKie" > /dev/null
+          curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1QKYG58WLDdq4ar690H5qCNiSgI1twKie" -o RGB_Dataset.zip
           rm cookie
-          rm -rf data
-          unzip data.zip -d data
-          rm data.zip; 
+          rm -rf data/RGB_Dataset
+          unzip RGB_Dataset.zip -d data
+          rm RGB_Dataset.zip; 
+          break;;
+      [Nn]* ) break;;
+      * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wish to download backbone checkpoints? [y/n]: " yn
+    case $yn in
+      [Yy]* )
+          curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1B53u3WApaicCsaLYZIKjNSrmhe8qKeGP" > /dev/null
+          curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1B53u3WApaicCsaLYZIKjNSrmhe8qKeGP" -o backbone_ckpt.zip
+          rm cookie
+          rm -rf data/backbone_ckpt
+          unzip backbone_ckpt.zip -d data
+          rm backbone_ckpt.zip; 
           break;;
       [Nn]* ) break;;
       * ) echo "Please answer yes or no.";;
@@ -34,7 +50,7 @@ while true; do
           curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1IlHzuFeAMbPzxLCghaFzDV1FPuXwwcC0" -o snapshots.zip
           rm cookie
           rm -rf snapshots
-          unzip snapshots.zip -d snapshots
+          unzip snapshots.zip
           rm snapshots.zip; 
           break;;
       [Nn]* ) break;;
