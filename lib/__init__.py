40af4fb85591f29a2b2049cc7cd969a5cb5f7da4
Taehun Kim <taehoon1018@postech.ac.kr>
2021-10-29 19:03:27 +0900

    code update

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,2 @@
-from lib.InSPyReNet import InSPyReNet_Res2Net50, InSPyReNet_SwinT, InSPyReNet_SwinS, InSPyReNet_SwinB, InSPyReNet_SwinL
+from lib.InSPyReNet import InSPyReNet_Res2Net50, InSPyReNet_Res2Net101, InSPyReNet_SwinT, InSPyReNet_SwinS, InSPyReNet_SwinB, InSPyReNet_SwinL
 from lib.InSPyReNet_Grid import InSPyReNet_Grid
